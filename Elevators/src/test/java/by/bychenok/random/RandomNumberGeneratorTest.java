e33b6ffed8424be26bc644286fe3688448c50bfa
Egor Bychenok <egorix2000@mail.ru>
2020-12-05 13:27:05 +0300

    add random number generatoe tests

--- Elevators/src/test/java/by/bychenok/random/RandomNumberGeneratorTest.java
+++ Elevators/src/test/java/by/bychenok/random/RandomNumberGeneratorTest.java
@@ -2,11 +2,67 @@ package by.bychenok.random;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static by.bychenok.random.RandomNumberGenerator.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RandomNumberGeneratorTest {
 
     @Test
-    void generateNumberInRangeWithoutValue() {
+    void generateNumberInRangeWithoutValue_minGreaterOrEqualsToMax_fail() {
+        //GIVEN
+        int minGreater = 10;
+        int minEquals = 9;
+        int max = 9;
+        int excludeValue = 9;
+
+        //EXPECT
+        assertThrows(IllegalArgumentException.class, () ->
+                generateNumberInRangeWithoutValue(minGreater, max, excludeValue));
+        //AND
+        assertThrows(IllegalArgumentException.class, () ->
+                generateNumberInRangeWithoutValue(minEquals, max, excludeValue));
+    }
+
+    @Test
+    void generateNumberInRangeWithoutValue_excludeValueNotInRange_fail() {
+        //GIVEN
+        int min = 5;
+        int max = 10;
+        int excludeValueLow = 4;
+        int excludeValueHigh = 10;
+
+        //EXPECT
+        assertThrows(IllegalArgumentException.class, () ->
+                generateNumberInRangeWithoutValue(min, max, excludeValueLow));
+        //AND
+        assertThrows(IllegalArgumentException.class, () ->
+                generateNumberInRangeWithoutValue(min, max, excludeValueHigh));
+    }
+
+    @Test
+    void generateNumberInRangeWithoutValue_excludeValueEqualsMin_success() {
+        //GIVEN
+        int min = 5;
+        int max = 10;
+        int excludeValue = 5;
+
+        //EXPECT
+        assertDoesNotThrow(() -> generateNumberInRangeWithoutValue(min, max, excludeValue));
+    }
+
+    @Test
+    void generateNumberInRangeWithoutValue_rangeSingleElement_fail() {
+        //GIVEN
+        int min = 1;
+        int max = 2;
+        int excludeValue = 1;
+
+        //EXPECT
+        assertThrows(IllegalArgumentException.class, () ->
+                generateNumberInRangeWithoutValue(min, max, excludeValue));
     }
 }
